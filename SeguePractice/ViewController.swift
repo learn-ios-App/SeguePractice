a3a0f1a90d0a4d1ee795b02b05daab95a1d1672e
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2023-02-09 21:03:08 +0900

    Initial Commit

---
+++ SeguePractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeguePractice
+//
+//  Created by 渡邊魁優 on 2023/02/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
